253336e9eba1c3efd7d673bc0869fb51947596cd
Prarthana B K <prarthana25305@gmail.com>
2025-01-12 17:03:56 +0530

    routes and changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Header = ({ toggleTimer, timerEnabled }) => {
   return (
-    <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">Pair It Up</h1>
-      
-    </header>
+    <>
+    <header className="bg-blue-500 text-white p-4 flex justify-between items-center mt-6 mb-6 ">
+      <h1 className="text-2xl font-bold">Pair It Up</h1>  <br />
+      </header>
+      <p className="bg-gray-400 text-white-300 p-5 flex justify-center items-center text-xl">Welcome to the world of Permutations & Combinations !!</p>
+      </>
   );
 };
 
 export default Header;
+ 
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -4,15 +4,12 @@ const Scoreboard = ({ score, totalCards }) => {
   const progress = (score / (totalCards * 10)) * 100;
 
   return (
-    <div className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <h2>Score: {score}</h2>
-      <div className="w-full bg-gray-600 h-2 rounded overflow-hidden ml-4">
+    <div className="text-white p-4 flex justify-between items-center">
         <div
           className="bg-yellow-500 h-full"
           style={{ width: `${progress}%` }}
         ></div>
       </div>
-    </div>
   );
 };
 
